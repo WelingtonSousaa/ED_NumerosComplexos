3774e2c0203e2b57cb2dc0646b5ab7e2296ad7af
Welington Sousa <wp.sousa.2004@gmail.com>
2024-04-22 20:08:29 -0300

    Mensagens de erro concluídas - Welington

--- complexo.c
+++ complexo.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
 #include "complexo.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
 struct complexo
 {
@@ -10,7 +10,6 @@ struct complexo
     float imaginario;
 };
 
-// Desenvolvida por Ícaro
 // Função para criar um número complexo na forma algébrica z = a + bi, com a e b sendo números reais.
 Complexo *criaComplexoFormaAlgebrica(float a, float b)
 {
@@ -28,9 +27,8 @@ Complexo *criaComplexoFormaAlgebrica(float a, float b)
         printf("Memória insuficiente");
         exit(1);
     }
-}
+} // Desenvolvida por Ícaro
 
-//  Desenvolvida por Ícaro
 // Função que cria um número complexo na forma polar z = a(cos(b) + sen(b)i), com a e b sendo números reais.
 Complexo *criaComplexoFormaPolar(float a, float b)
 {
@@ -48,9 +46,8 @@ Complexo *criaComplexoFormaPolar(float a, float b)
         printf("Memória insuficiente");
         exit(1);
     }
-}
+} //  Desenvolvida por Ícaro
 
-// Desenvolvida por Ícaro...
 // Função que libera a memória alocada para um número complexo.
 void liberaComplexo(Complexo *z)
 {
@@ -62,9 +59,8 @@ void liberaComplexo(Complexo *z)
     {
         printf("Liberação do número inválida!\n");
     }
-}
+} // Desenvolvida por Ícaro...
 
-// Desenvolvida por Ícaro
 // Função que retorna a parte real a e a parte imaginária b do número complexo z na forma algébrica z = a + bi, com a e b sendo números reais.
 void formaAlgebrica(Complexo *z, float *a, float *b)
 {
@@ -77,9 +73,8 @@ void formaAlgebrica(Complexo *z, float *a, float *b)
     {
         printf("Forma algébrica do número inválida!\n");
     }
-}
+} // Desenvolvida por Ícaro
 
-// Desenvolvida por Ícaro
 // Função que retorna o módulo a e o argumento b do número complexo z na forma polar z = a(cos(b) + sen(b)i), com a e b sendo números reais.
 void formaPolar(Complexo *z, float *a, float *b)
 {
@@ -92,9 +87,8 @@ void formaPolar(Complexo *z, float *a, float *b)
     {
         printf("Forma polar do número inválida!\n");
     }
-}
+} // Desenvolvida por Ícaro
 
-// Desenvolvido por Welington
 //  Função que verifica se a parte real de um número complexo é igual a zero, retornando 1 caso afirmativo e 0 caso contrário.
 int testeReal(Complexo *z)
 {
@@ -113,9 +107,8 @@ int testeReal(Complexo *z)
     {
         printf("O endereço do número complexo não pode ser nulo");
     }
-}
+} // Desenvolvido por Welington
 
-// Desenvolvido por Welington
 // Função que verifica se a parte imaginária de um número complexo é igual a zero, retornando 1 caso afirmativo e 0 caso contrário.
 int testeImaginario(Complexo *z)
 {
@@ -134,9 +127,8 @@ int testeImaginario(Complexo *z)
     {
         printf("O endereço do número complexo não pode ser nulo");
     }
-}
+} // Desenvolvido por Welington
 
-// Desenvolvido por Welington
 // Função que verifica se tanto a parte real quanto a parte imaginária de um número complexo são iguais a zero, retornando 1 caso afirmativo e 0 caso contrário.
 int testeZero(Complexo *z)
 {
@@ -155,9 +147,8 @@ int testeZero(Complexo *z)
     {
         printf("O endereço do número complexo não pode ser nulo");
     }
-}
+} // Desenvolvido por Welington
 
-// Desenvolvido por Welington
 // Retorna um número complexo oriundo da soma z = z1 + z2, com z1 e z2 sendo números complexos.
 Complexo *addComplexo(Complexo *z1, Complexo *z2)
 {
@@ -166,55 +157,108 @@ Complexo *addComplexo(Complexo *z1, Complexo *z2)
         Complexo *resultado = (Complexo *)malloc(sizeof(Complexo));
         if (resultado != NULL)
         {
-            resultado->real = z1->real + z2->real;
-            resultado->imaginario = z1->imaginario + z2->imaginario;
+            if (resultado != NULL)
+            {
+                resultado->real = z1->real + z2->real;
+                resultado->imaginario = z1->imaginario + z2->imaginario;
+                return resultado;
+            }
         }
-        return resultado;
+        else
+        {
+            printf("Memória insuficiente!");
+            return NULL;
+        }
     }
     else
     {
         printf("Os endereços de ambos os números complexos não podem ser nulos");
+        return NULL;
     }
-}
+} // Desenvolvido por Welington
 
-// Desenvolvido por Welington
 // Retorna um número complexo oriundo da subtração z = z1 - z2, com z1 e z2 sendo números complexos
 Complexo *subComplexo(Complexo *z1, Complexo *z2)
 {
-    Complexo *resultado = (Complexo *)malloc(sizeof(Complexo));
-    if (resultado != NULL)
+    if (z1 != NULL && z2 != NULL)
     {
-        resultado->real = z1->real - z2->real;
-        resultado->imaginario = z1->imaginario - z2->imaginario;
+        Complexo *resultado = (Complexo *)malloc(sizeof(Complexo));
+        if (resultado != NULL)
+        {
+            if (resultado != NULL)
+            {
+                resultado->real = z1->real - z2->real;
+                resultado->imaginario = z1->imaginario - z2->imaginario;
+                return resultado;
+            }
+        }
+        else
+        {
+            printf("Memória Insuficiente!");
+            return NULL;
+        }
     }
-    return resultado;
-}
+    else
+    {
+        printf("Os endereços de ambos os números complexos não podem ser nulos");
+        return NULL;
+    }
 
-// Desenvolvido por Welington
+} // Desenvolvido por Welington
+
 // Retorna um número complexo oriundo da multiplicação z = z1 * z2, com z1 e z2 sendo números complexos
 Complexo *multComplexo(Complexo *z1, Complexo *z2)
 {
-    Complexo *resultado = (Complexo *)malloc(sizeof(Complexo));
-    if (resultado != NULL)
+    if (z1 != NULL && z2 != NULL)
     {
-        resultado->real = z1->real * z2->real - z1->imaginario * z2->imaginario;
-        resultado->imaginario = z1->real * z2->imaginario + z1->imaginario * z2->real;
+        Complexo *resultado = (Complexo *)malloc(sizeof(Complexo));
+        if (resultado != NULL)
+        {
+            if (resultado != NULL)
+            {
+                resultado->real = z1->real * z2->real - z1->imaginario * z2->imaginario;
+                resultado->imaginario = z1->real * z2->imaginario + z1->imaginario * z2->real;
+                return resultado;
+            }
+        }
+        else
+        {
+            printf("Memória Insuficiente!");
+            return NULL;
+        }
     }
-    return resultado;
-}
+    else
+    {
+        printf("Os endereços de ambos os números complexos não podem ser nulos");
+        return NULL;
+    }
+} // Desenvolvido por Welington
 
 // Desenvolvido por Welington
 // Retorna um número complexo oriundo da divisão z = z1 / z2, com z1 e z2 sendo números complexos e z2 diferente de zero.
 Complexo *divComplexo(Complexo *z1, Complexo *z2)
 {
-    Complexo *resultado = (Complexo *)malloc(sizeof(Complexo));
-    if (resultado != NULL)
+    if (z1 != NULL && z2 != NULL)
     {
-        float divisor = z2->real * z2->real + z2->imaginario * z2->imaginario;
-        resultado->real = (z1->real * z2->real + z1->imaginario * z1->imaginario) / divisor;
-        resultado->imaginario = (z1->imaginario * z2->real - z1->real * z2->imaginario) / divisor;
+        Complexo *resultado = (Complexo *)malloc(sizeof(Complexo));
+        if (resultado != NULL)
+        {
+            float divisor = z2->real * z2->real + z2->imaginario * z2->imaginario;
+            resultado->real = (z1->real * z2->real + z1->imaginario * z1->imaginario) / divisor;
+            resultado->imaginario = (z1->imaginario * z2->real - z1->real * z2->imaginario) / divisor;
+            return resultado;
+        }
+        else
+        {
+            printf("Memória Insuficiente");
+            return NULL;
+        }
     }
-    return resultado;
+    else
+    {
+        printf("Os endereços de ambos os números complexos não podem ser nulos");
+        return NULL;
+    }
 }
 
 // Em desenvolvimento por {Guilherme}...
